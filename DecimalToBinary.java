bf9cca9cce281c66c3afcce0e4d6cc55442f9016
Mahaveer K <mahaveer.k2023it@sece.ac.in>
2026-06-15 22:38:13 +0530

    problem no.6

--- DecimalToBinary.java
+++ DecimalToBinary.java
@@ -4,14 +4,14 @@ public class DecimalToBinary {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        // Input decimal number
+        
         System.out.print("Input a Decimal Number: ");
         int decimal = sc.nextInt();
 
-        // Convert to binary
+        
         String binary = Integer.toBinaryString(decimal);
 
-        // Display result
+        
         System.out.println("Binary number is: " + binary);
 
         sc.close();
